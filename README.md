e305629490701eb06b9753edfd733703051d0111
Gustavo Andrade <gusthliv@gmail.com>
2018-11-25 00:06:15 -0200

    unity branch with roll a ball

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# fcc
+# unity
 
-Free Code Camp Projects
+Unity Projects
 
 ## How to run
 
 1. Clone the repository with `git clone git@github.com:gusthliv/gusthliv.git`
 2. Change directory with `cd gusthliv.com`
-3. Checkout fcc branch with `git checkout fcc`
+3. Checkout unity branch with `git checkout unity`
 4. Install packages with `npm install`
 5. Run with `npm run start`
---
+++ src/Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+  public float speed;
+  public Text pickUpCountText;
+  public Text winText;
+
+  private new Rigidbody rigidbody;
+  private int pickUpCount;
+
+  void Start() {
+    rigidbody = GetComponent<Rigidbody>();
+    pickUpCount = 0;
+    UpdatePickUpCountText();
+    winText.text = "";
+  }
+
+  void FixedUpdate() {
+    rigidbody.AddForce(new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical")) * speed);
+  }
+
+  void OnTriggerEnter(Collider other) {
+    if (other.gameObject.CompareTag("Pick Up")) {
+      other.gameObject.SetActive(false);
+      pickUpCount++;
+      UpdatePickUpCountText();
+    }
+  }
+
+  void UpdatePickUpCountText() {
+    pickUpCountText.text = "Pick Up Count: " + pickUpCount.ToString();
+
+    if (pickUpCount >= 12) {
+      winText.text = "YOU WIN";
+    }
+  }
+}
